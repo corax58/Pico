eee2d4f291e75255c927fb6b9334d0a2f2cf914d
corax <abubekera87@gmail.com>
2024-05-29 00:02:43 +0300

    fixed columns

--- Pico/src/main.jsx
+++ Pico/src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/router.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router}/>
-
+      <RouterProvider router={router} />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- Pico/src/pages/HomePage.jsx
+++ Pico/src/pages/HomePage.jsx
@@ -1,25 +1,43 @@
-import PostCard from "@/components/PostCard"
-import useFetchAllPosts from "@/hooks/useFetchAllPosts"
+import PostCard from "@/components/PostCard";
+import useFetchAllPosts from "@/hooks/useFetchAllPosts";
 
-const HomePage = () => { 
-const {data,isLoading}=useFetchAllPosts()
+const HomePage = () => {
+  const { data, isLoading, error } = useFetchAllPosts();
 
-console.log(data)
+  console.log(data);
 
-if(isLoading) return <h1>Loading...</h1>
+  if (isLoading)
+    return (
+      <div className="w-full justify-center flex mt-32">
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
+    );
+  if (error)
+    return (
+      <div role="alert" className="alert alert-error">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="stroke-current shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>{error.message}</span>
+      </div>
+    );
   return (
-    <div className="   space-y-5  justify-evenly      columns-6 p-5 ">
-    
- {
-  data.map((post)=> <PostCard key={post._id} post={post}/>)
- }
-
+    <div className="   space-y-5  justify-center items-center  md:justify-evenly   md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6  columns-1 p-5 ">
+      {data.map((post) => (
+        <PostCard key={post._id} post={post} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-
-
-
-
-export default HomePage
+export default HomePage;
